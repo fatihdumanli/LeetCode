8abaa6804d6dfb6c1fea67872dfe53fcef9280c0
Fatih <fatihdumanli@live.com>
2021-11-17 15:16:13 +0300

    Add PalindromeNubmer

---
+++ PalindromeNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PalindromeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"[{args[0]}] Is Palindrome?: {IsPalindrome(Convert.ToInt32(args[0]))}");
+        }
+
+        static bool IsPalindrome(int x)
+        {
+            var str = x.ToString();
+            var length = str.Length;
+        
+            int middle = length / 2;
+        
+            int i = 0;
+        
+            while(i < middle)
+            {
+                if(str[i] != str[length - 1 - i])
+                {
+                    return false;
+                }
+                i++;            
+            }
+        
+            return true;
+
+        }
+    }
+}
