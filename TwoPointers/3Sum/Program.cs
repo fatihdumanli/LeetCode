8fea1896895388aed5bba00162041772774a49ae
= <fatihdumanli@live.com>
2021-11-27 19:52:15 +0300

    Move LPS to TwoPointesr

---
+++ TwoPointers/3Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[]
+            {
+                -1, 0, 1, 2, 1, -4
+            };
+
+            var result = ThreeSum(nums);
+            
+            Console.WriteLine("Hello World!");
+        }
+        
+        static IList<IList<int>> ThreeSum(int[] nums)
+        {
+            return null;
+        }
+    }
+}
