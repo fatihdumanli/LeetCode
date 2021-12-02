7d428a9869ae6aa651c6a1f972613677f1a5cc5f
= <fatihdumanli@live.com>
2021-12-02 19:45:27 +0300

    Add problem ProductOfArrayExceptSelf

---
+++ Arrays/ProductOfArrayExceptSelf/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ProductOfArrayExceptSelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[]
+            {
+                1, 2, 3, 4                
+            };
+
+            var result = ProductExceptSelf(nums);
+            
+            Console.WriteLine("Hello World!");
+        }
+
+        static int[] ProductExceptSelf(int[] nums)
+        {
+            int[] prefix = new int[nums.Length + 1];
+            int[] postfix = new int[nums.Length + 1];
+            prefix[0] = 1;
+            postfix[nums.Length] = 1;
+        
+            int[] result = new int[nums.Length];
+        
+            int product = 1;
+        
+            for(int i = 0; i < nums.Length; i++)
+            {
+                prefix[i + 1] = product *= nums[i];
+            }
+        
+            product = 1;
+        
+            for(int i = nums.Length - 1; i >= 0; i--)
+            {
+                postfix[i] = product *= nums[i];
+                result[i] = postfix[i + 1] * prefix[i];
+            }
+        
+            return result;               
+        }
+    }
+}
