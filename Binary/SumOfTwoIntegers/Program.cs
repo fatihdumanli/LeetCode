78ce262d9b24292693aa9647117890a9739d79ec
= <fatihdumanli@live.com>
2021-12-08 14:37:14 +0300

    Add LongestRepeatingCharacterReplacement.

---
+++ Binary/SumOfTwoIntegers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SumOfTwoIntegers3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static int Add(int a, int b)
+        {
+            while (b != 0)
+            {
+                var tmp = (a & b) << 1;
+                a = a ^ b;
+                b = tmp;
+            }
+
+            return a;
+        }
+    }
+}
---
+++ String/LongestRepeatingCharacterReplacement/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LongestRepeatingCharacterReplacement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = "ABBB";
+            //"AABABBBBBB"
+            //1
+
+            var result = CharacterReplacement(str, 2);
+            Console.WriteLine("Hello World!");
+        }
+        
+        static int CharacterReplacement(string s, int k) {
+                
+            Dictionary<char, int> frequency = new Dictionary<char, int>();
+
+            char[] englishCharacters = new char[] {
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 
+                'Q','W','U', 'X','V', 'Y', 'Z'
+            };
+        
+            foreach(var c in englishCharacters)
+                frequency.Add(c, 0);
+            
+            int left = 0;
+            int right = 0;
+            int max = 0;    
+        
+            // 26 + O(N * 26) = O(26*N) = O(N) 
+            int maxFrequency = 0;
+
+            while(right < s.Length)
+            {        
+                frequency[s[right]]++;
+            
+            
+                foreach(var item in frequency)
+                    maxFrequency = Math.Max(maxFrequency, item.Value);
+            
+                var substringLength = right - left + 1;
+            
+                //valid substring
+                if(substringLength - maxFrequency <= k)
+                {
+                    max = Math.Max(max, substringLength);
+                    right++;
+                }
+            
+                else 
+                {
+                    frequency[s[left]]--;
+                    frequency[s[right]]--;
+                    left++;
+                }    
+            
+            }
+        
+            return max;
+        }
+    }
+}
