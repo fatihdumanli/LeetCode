fe33d19dcc396e39d4428d1661090ea381fdfebd
= <fatihdumanli@live.com>
2021-12-08 15:34:23 +0300

    add IsAnagram

---
+++ String/IsAnagram/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace IsAnagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "anagram";
+            var t = "naagram";
+            Console.WriteLine(IsAnagram(s, t));
+        }
+        
+        static bool IsAnagram(string s, string t) {
+       
+            int[] frequency = new int[26];
+        
+            foreach(var c in s)
+                frequency[(int)c - 97]++;
+    
+            foreach(var c in t)
+                frequency[(int)c - 97]--;
+                  
+            for(int i = 0; i < 26; i++)
+                if(frequency[i] != 0) return false;
+        
+            return true;        
+        
+        }
+    }
+}
