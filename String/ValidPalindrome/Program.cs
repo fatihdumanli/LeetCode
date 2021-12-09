994acbf30d155d0bcf7b81e04639dce77f31e7fd
= <fatihdumanli@live.com>
2021-12-09 16:55:02 +0300

    add valid paranthesis and valid palindrome

---
+++ String/ValidPalindrome/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ValidPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "racecar";
+            
+            // racecar
+            Console.WriteLine(IsPalindrome(s));
+        }
+        
+        static bool IsPalindrome(string s) {
+        
+            int left = 0;
+            int right = s.Length - 1;
+        
+            //raceacar
+        
+            while(left < right) {
+                if(!IsAlphanumeric(s[left])) { left++; continue; }
+                if(!IsAlphanumeric(s[right])) { right--; continue; }
+            
+                if(Char.ToLower(s[left]) != Char.ToLower(s[right]))
+                    return false;
+            
+                left++;
+                right--;
+            }
+        
+            return true;
+        
+        }
+    
+        static bool IsAlphanumeric(char c) {
+            var ascii = (int)c;
+            return (ascii >= 48 && ascii <= 57) || (ascii >= 65 && ascii <= 90) || (ascii >= 97 && ascii <= 122);
+        }
+    }
+}
---
+++ String/ValidParentheses/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ValidParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "}}";
+            var result = IsValid(s);
+            Console.WriteLine("Hello World!");
+        }
+        
+        static bool IsValid(string s) {
+              
+                char[] stack = new char[s.Length];
+                int current = 0;
+                //}}
+                for(int i = 0; i < s.Length; i++)
+                {
+                    if(s[i] == '(' || s[i] == '[' || s[i] == '{') {
+                        stack[current++] = s[i];
+                        continue;
+                    }
+
+                    switch (s[i])
+                    {
+                        case ')':
+                            if (current == 0 || stack[--current] != '(') return false;
+                            break;
+                        case '}':
+                            if (current == 0 || stack[--current] != '{') return false;
+                            break;
+                        case ']':
+                            if (current == 0 || stack[--current] != '[') return false;
+                            break;
+                    }
+                }
+                
+                return true;
+                
+            }
+    }
+}
