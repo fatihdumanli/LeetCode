925b519e914a7782379b1be2837f226bd458be76
= <fatihdumanli@live.com>
2021-12-10 19:13:28 +0300

    Add StringBuilder

--- Implementations/DesignTests/Program.cs
+++ Implementations/DesignTests/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using HashMap;
 using HashSet;
 using List;
+using StringBuilder;
 
 namespace DesignTests
 {
@@ -10,6 +11,7 @@ namespace DesignTests
     {
         static void Main(string[] args)
         {
+
             var contains = false;
             var list = new MyList(2);
             list.Add(2);
@@ -44,6 +46,36 @@ namespace DesignTests
 
             var result = hashmap.Get(2);
             var result2 = hashmap.Get(9);
+
+            var words = new[] { "foo", "bar", "all", "key" };
+            BuildSentence(words);
+        }
+
+
+        static void BuildSentence(string[] words)
+        {
+            string sentence = "";
+            var chars = sentence.ToCharArray();
+
+            foreach (var word in words)
+            {
+                var temp = word.ToCharArray();
+                sentence += word;
+                var temp2 = sentence.ToCharArray();
+                // 3 + 6  + 9  + 12 + 15
+                // x + 2x + 3x + 4x + 5x
+                // x(1 + 2 + 3 + 4 + 5)
+                // x * (n * (n-1)) / 2
+                // x * n^2
+                // O(xn^2)
+            }
+
+            var stringBuilder = new MyStringBuilder();
+
+            foreach (var word in words)
+                stringBuilder.Append(word);
+
+            var result = stringBuilder.ToString();
         }
     }
 }
---
+++ Implementations/StringBuilder/MyStringBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace StringBuilder
+{
+    public class MyStringBuilder
+    {
+        private const int ARRAY_SIZE = 50;
+        private char[] _array = new char[ARRAY_SIZE];
+        private int _ptr = 0;
+        
+        public void Append(string phrase)
+        {
+            if (_ptr + phrase.Length > _array.Length)
+            {
+                Resize();
+            }
+            foreach (var c in phrase)
+                _array[_ptr++] = c;
+        }
+
+        private void Resize()
+        {
+            var temp = _array;
+            _array = new char[_array.Length * 2];
+
+            for (int i = 0; i < _ptr; i++)
+            {
+                _array[i] = temp[i];
+            }
+        }
+        
+        public override string ToString()
+        {
+            return new string(_array);
+        }
+    }
+}
