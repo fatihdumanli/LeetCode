bb70a80c5776711a7b91ddf73dfcdc375fb23872
= <fatihdumanli@live.com>
2021-12-14 17:02:21 +0300

    Add OneWay

--- CTCI/StringAndArrays/OneAway/Program.cs
+++ CTCI/StringAndArrays/OneAway/Program.cs
@@ -6,33 +6,60 @@ namespace OneAway
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var result = OneAway("pale", "ple");
+            Console.WriteLine(result);
+
+            result = OneAway("pales", "pale");
+            Console.WriteLine(result);
+
+            result = OneAway("pale", "bale");
+            Console.WriteLine(result);
+            
+            result = OneAway("pale", "bake");
+            Console.WriteLine(result);
         }
 
 
         static bool OneAway(string s1, string s2)
         {
-            int[] charset1 = new int[26];
-            int[] charset2 = new int[26];
-            
-            foreach (var c in s1)
-                charset1[c - 'a']++;
+            if (Math.Abs(s1.Length - s2.Length) > 1)
+                return false;
 
-            foreach (var c in s2)
-                charset2[c - 'a']++;
-
-
-            int charactersOff = 0;
-            
-            for (int i = 0; i < 26; i++)
+            if (s1.Length == s2.Length)
             {
-                if (charset1[i] != charset2[i])
-                    charactersOff++;
-
-                if (charactersOff > 1)
-                    return false;
+                //Character by character comparison
+                bool mismatch = false;
+                for (int i = 0; i < s1.Length; i++)
+                {
+                    if (s1[i] == s2[i]) continue;
+                    if (mismatch)
+                        return false;
+                    mismatch = true;
+                }   
             }
+            
+            else
+            {
+                //ASCII check
+                var charset = new int[26];
+                foreach (var c in s1)
+                    charset[c - 'a']++;
 
+                var charset2 = new int[26];
+                var mismatch = false;
+                foreach (var c in s2)
+                {
+                    charset2[c - 'a']++;
+
+                    if (charset2[c - 'a'] == charset[c - 'a']) continue;
+                    
+                    if (mismatch)
+                        return false;
+                    mismatch = true;
+                }
+            }
+            
             return true;
         }
     }
