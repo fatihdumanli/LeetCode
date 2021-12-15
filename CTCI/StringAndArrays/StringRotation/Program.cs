fb44a2b4432576fbc18c25706cff0a6c87d80854
= <fatihdumanli@live.com>
2021-12-15 11:21:06 +0300

    Add StringRotation

---
+++ CTCI/StringAndArrays/StringRotation/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace StringRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s1 = "waterbottle";
+            var s2 = "erbottlewat";
+            var result = IsRotation(s1, s2);
+        }
+
+        static bool IsRotation(string s1, string s2)
+        {
+            return (s1.Length <= s2.Length) && (s2 + s2).Contains(s1);
+        }
+    }
+}
