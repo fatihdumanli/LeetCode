13d147af04d4d94231f42422df469e98d9272748
= <fatihdumanli@live.com>
2021-12-19 15:17:26 +0300

    Add MergeKSortedLists and ReorderList

---
+++ LinkedList/MergeKSortedLists/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Common;
+
+namespace MergeKSortedLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var node1 = new ListNode(2);
+            node1.next = new ListNode(1);
+            
+            var node2 = new ListNode(4);
+            node2.next = new ListNode(3);
+            
+
+            var node3 = new ListNode(2);
+            node3.next = new ListNode(6);
+
+            var list = new ListNode[]
+            {
+                node1, node2, node3
+            };
+
+            var result = MergeKLists(list);
+            Console.WriteLine("Hello World!");
+        }
+        
+        static ListNode MergeKLists(ListNode[] lists) {
+            ListNode sorted = new ListNode();
+
+
+            foreach (var node in lists)
+            {
+                var ptr = node;
+                while (ptr != null)
+                {
+                    sorted = InsertNode(sorted, ptr);
+                    ptr = ptr.next;
+                }
+            }
+            
+            return sorted.next;
+        }
+        
+        
+        static ListNode InsertNode(ListNode sorted, ListNode node)
+        {
+            ListNode headCopy = sorted;
+
+            if (sorted.next == null)
+            {
+                sorted.next = new ListNode(node.val);
+                return headCopy;
+            }
+
+            while(sorted.next != null && node.val >= sorted.next.val)
+            {
+                sorted = sorted.next;
+            }
+
+            var temp = sorted.next;
+            sorted.next = new ListNode(node.val);
+            sorted.next.next = temp;
+    
+            return headCopy;
+     
+        }
+        
+    }
+}
--- LinkedList/ReorderList/Program.cs
+++ LinkedList/ReorderList/Program.cs
@@ -1,56 +1,144 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using Common;
 
 namespace ReorderList
 {
-    public class ReorderWrapper 
-    {
-        public ListNode originalHead;
-        public ListNode tailPtr;
-        public ListNode headPtr;
+    public class LastElementWrapper {
+        public ListNode newHead;
+        public int lastNodeVal;
     }
     
     class Program
     {
         static void Main(string[] args)
         {
+            
             var head = new ListNode(1);
-            head.next = new ListNode(2);
-            head.next.next = new ListNode(3);
-            head.next.next.next = new ListNode(4);
+
+            var headPtr = head;
+            for (int i = 0; i < 1000; i++)
+            {
+                headPtr.next = new ListNode(i);
+                headPtr = headPtr.next;
+            }
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            ReorderListPtrs(head);
+
+            stopwatch.Stop();
+            Console.WriteLine($"Solution with multiple ptrs took {stopwatch.ElapsedTicks} ticks");
 
             
+            head = new ListNode(1);
+            headPtr = head;
+            for (int i = 0; i < 1000; i++)
+            {
+                headPtr.next = new ListNode(i);
+                headPtr = head.next;
+            }
+            stopwatch.Reset();
+            stopwatch.Start();
             ReorderList(head);
-            Console.WriteLine("Hello World!");
+            stopwatch.Stop();
+            Console.WriteLine($"Solution with stack/queue took {stopwatch.ElapsedTicks} ticks");
+
+
+            
         }
-        
-        static void ReorderList(ListNode head) {
-            var wrapper = new ReorderWrapper() {
-                originalHead = head,
-                headPtr = head
-            };
-        
+
+        static void ReorderListPtrs(ListNode head)
+        {
+           
+            if(head.next == null)
+                return;        
+   
             var tailPtr = head;
         
-            var result = ReorderHelper(tailPtr, wrapper);
-        }
-        
-        static ReorderWrapper ReorderHelper(ListNode tailPtr, ReorderWrapper wrapper)
-        {
-            if (tailPtr.next == null)
+            /* finding length */
+            int length = 0;
+            while(tailPtr != null)
             {
-                
+                length++; 
+                tailPtr = tailPtr.next;
             }
         
-            var result = ReorderHelper(tailPtr.next, wrapper);
+            var startPtr = head.next;
+            head.next = null;
+            length--;
         
-            var temp = wrapper.headPtr.next;
-            wrapper.headPtr.next = tailPtr;
-            wrapper.headPtr.next.next = temp;
-            wrapper.headPtr = wrapper.headPtr.next.next;
+            var orgHeadPtr = head;
         
+    
+            while(length > 0)
+            {
+                tailPtr = startPtr;
+            
+                for(int i = 0; i < length - 1; i++)
+                    tailPtr = tailPtr.next;
+            
+                orgHeadPtr.next = new ListNode(tailPtr.val);
+            
+                if(length == 1)
+                    break;
+            
+                orgHeadPtr.next.next = new ListNode(startPtr.val);
+                orgHeadPtr = orgHeadPtr.next.next;
+           
+                startPtr = startPtr.next;
+                length -= 2;
+            }
+
+            var x = 2;
+
+        }
+
+        static void ReorderList(ListNode head)
+        {
+            if(head.next == null)
+                return;        
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            ListNode p1 = head;
+            ListNode p2 = slow;
+            Queue<int> queue = new Queue<int>();
+            Stack<int> stack = new Stack<int>();    
+
+            while (p1 != slow)
+            {
+                queue.Enqueue(p1.val);
+                p1 = p1.next;    
+            }
         
-            return wrapper;
+            
+            while (p2 != null)
+            {
+                stack.Push(p2.val);
+                p2 = p2.next;    
+            }
+        
+            queue.Dequeue();       
+            while(queue.Count > 0)
+            {
+                var popped = stack.Pop();
+                head.next = new ListNode(popped);
+                var dequeued = queue.Dequeue();
+                head.next.next = new ListNode(dequeued);
+                head = head.next.next;
+            }     
+            head.next = new ListNode(stack.Pop());
+            if(stack.Count > 0)
+                head.next.next = new ListNode(stack.Pop());
+
         }
     }
 }
