f01becee8bb243b118d80551990ca1d97ac95ba9
Fatih Dumanli <fatihdumanli@live.com>
2022-07-20 22:48:09 +0200

    Add IsUnique

---
+++ ctci/stringandarrays/isunique/cs/Program.cs
@@ -0,0 +1,23 @@
+﻿string s = "rotterdam";
+Console.WriteLine(IsUnique(s));
+
+// Return true if the string consists of unique characters.
+bool IsUnique(string s)
+{
+    if (s.Length > 128) 
+        return false;
+
+    bool[] freq = new bool[128];
+
+    for(int i = 0; i < s.Length; i++)
+    {
+        char c = s[i];
+
+        if(freq[c])
+            return false;
+        
+        freq[c] = true;
+    }
+
+    return true;
+}
