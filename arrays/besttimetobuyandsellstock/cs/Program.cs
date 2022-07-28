9b9d92c7b649bdaef1fa95c2c9b52e61f3c61b87
Fatih Dumanli <fatihdumanli@live.com>
2022-07-28 22:22:55 +0200

    Add Best Time to Buy and Sell Stocks

---
+++ arrays/besttimetobuyandsellstock/cs/Program.cs
@@ -0,0 +1,18 @@
+﻿//https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+var prices = new int[] { 7, 1, 5, 3, 6, 4 };
+var result = MaxProfit(prices);
+Console.WriteLine(result);
+
+int MaxProfit(int[] prices)
+{
+    var minSoFar = prices[0];
+    var maxProfit = 0;
+
+    for(int i = 0; i < prices.Length; i++)
+    {
+        maxProfit = Math.Max(maxProfit, prices[i] - minSoFar);
+        minSoFar = Math.Min(minSoFar, prices[i]);
+    }
+    return maxProfit;
+}
