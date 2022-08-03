c89c78b8566c97cd31aa4297aa828a52a928f3e3
Fatih Dumanli <fatihdumanli@live.com>
2022-08-04 01:15:17 +0200

    Add LongestSubstringWithoutRepeatingCharactesr

---
+++ string/longestsubstringwithoutrepeatingcharacters/cs/Program.cs
@@ -0,0 +1,36 @@
+﻿var s = "tmmzuxt";
+var result = LengthOfLongestSubstring(s);
+Console.WriteLine(result);
+
+int LengthOfLongestSubstring(string s)
+{
+    if(s.Length == 0) {
+        return 0;
+    }
+
+    if(s.Length == 1) {
+        return 1;
+    }
+
+    var maxLength = 1;
+    var hashmap = new Dictionary<char, int>();
+    var left = 0;
+    var right = 1;
+    hashmap.Add(s[left], left);
+
+
+    while(left <= right && right <= s.Length - 1) 
+    {
+        if(!hashmap.ContainsKey(s[right])) 
+        {
+            hashmap.Add(s[right], right);
+            maxLength = Math.Max(maxLength, right - left + 1);
+            right++;
+        } else {
+            hashmap.Remove(s[left]);
+            left++;
+        }
+    }
+
+    return maxLength;
+}
