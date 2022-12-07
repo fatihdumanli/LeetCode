4f041e8684f9963acae221a94f7d6b448eb3506a
Fatih Dumanli <fatihdumanli@live.com>
2022-12-07 16:09:31 +0300

    Add range sum of bst

---
+++ tree/rangesumofbst/solution.go
@@ -0,0 +1,37 @@
+package main
+
+//https://leetcode.com/problems/range-sum-of-bst/
+func main() {
+}
+
+// inclusive!
+func rangeSumBST(root *TreeNode, low int, high int) int {
+	if root == nil {
+		return 0
+	}
+
+	var sum = 0
+
+	if root.Val >= low && root.Val <= high {
+		sum += root.Val
+	}
+
+	l := 0
+	r := 0
+
+	if low <= root.Val {
+		l = rangeSumBST(root.Left, low, high)
+	}
+
+	if high >= root.Val {
+		r = rangeSumBST(root.Right, low, high)
+	}
+	
+	return sum + l + r
+}
+
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
