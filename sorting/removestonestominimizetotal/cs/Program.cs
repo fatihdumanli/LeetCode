a3ad9c175d193c241f9a9b7d23f210f80bfbae34
Fatih Dumanli <fatihdumanli@live.com>
2022-12-28 01:55:18 +0300

    Add Remove stoenes to minimize total

---
+++ sorting/removestonestominimizetotal/cs/Program.cs
@@ -0,0 +1,25 @@
+﻿var piles = new int[] { 5, 4, 9 };
+var k = 2;
+var r = MinStoneSum(piles, k);
+Console.WriteLine(r);
+
+int MinStoneSum(int[] piles, int k)
+{
+    var sum = 0;
+    var pq = new PriorityQueue<int, int>();
+    foreach (var p in piles)
+    {
+        pq.Enqueue(p, 0 - p);
+        sum += p;
+    }
+
+    for (int i = 0; i < k; i++)
+    {
+        var max = pq.Dequeue();
+        sum -= (int)Math.Floor((decimal)max / 2);
+        max -= (int)Math.Floor((decimal)max / 2);
+        pq.Enqueue(max, 0 - max);
+    }
+
+    return sum;
+}
