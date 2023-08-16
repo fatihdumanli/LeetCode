09a3ec810a986ee7c4651ab258d0b7032eaef457
Fatih Dumanli <fatihdumanli@live.com>
2023-08-16 22:09:09 +0300

    Add FindTheIndexOfFirstOccurence

---
+++ string/firstoccurenceofsubstring/cs/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var haystack = "mississippi";
+var needle = "issip";
+
+var r = StrStr(haystack, needle);
+
+Console.WriteLine(r);
+
+//https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string
+static int StrStr(string haystack, string needle)
+{
+    // Optimized brute-force solution
+    for(int i = 0; i < haystack.Length; i++)
+    {
+        if(haystack[i] == needle[0])
+        {
+            var needlePtr = 0;
+            var haystackPtr = i;
+
+            while(haystackPtr <= haystack.Length - 1 && needlePtr <= needle.Length - 1 && haystack[haystackPtr] == needle[needlePtr])
+            {
+                needlePtr++;
+                haystackPtr++;
+            }
+            
+            if(needlePtr == needle.Length)
+                return haystackPtr - needle.Length;
+        }
+    }
+    
+    return -1;
+}
