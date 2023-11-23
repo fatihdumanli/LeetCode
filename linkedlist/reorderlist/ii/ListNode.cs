5e2c895e21e8b6163e716607a90cf5a46be0ccd3
Fatih Dumanli <fatihdumanli@live.com>
2023-11-23 19:58:57 +0300

    Add reorderlist queue/stack solution

---
+++ linkedlist/reorderlist/ii/ListNode.cs
@@ -0,0 +1,12 @@
+namespace ReorderList;
+
+public class ListNode
+{
+    public int val { get; set; }
+    public ListNode next { get; set; }
+
+    public ListNode(int val)
+    {
+        this.val = val;
+    }
+}
