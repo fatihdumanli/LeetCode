9a53c22a140b3137b5f389946faaf5967c993f0e
Fatih Dumanli <fatihdumanli@live.com>
2023-11-23 21:33:39 +0300

    Add SetMatrixZeroes

---
+++ matrix/setmatrixzeroes/cs/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var matrix = new int[][]
+{
+    new int[] { 1, 1, 1 },
+    new int[] { 1, 0, 1 },
+    new int[] { 1, 1, 1 }
+};
+
+SetZeroes(matrix);
+
+Console.WriteLine("done");
+
+void SetZeroes(int[][] matrix)
+{
+    List<int[]> zeroPositions = new List<int[]>();
+
+    for(int i = 0; i < matrix.Length; i++)
+    {
+        for(int j = 0; j < matrix[i].Length; j++)
+        {
+            if(matrix[i][j] == 0)
+                zeroPositions.Add(new int[] { i, j });
+        }
+    }
+
+    foreach(var z in zeroPositions)
+    {
+        var row = z[0];
+        var col = z[1];
+
+        for(int i = 0; i < matrix[row].Length; i++)
+        {
+            matrix[row][i] = 0;
+        }
+
+        for(int i = 0; i < matrix.Length; i++)
+        {
+            matrix[i][col] = 0;
+        }
+    }
+}
+
+
+
