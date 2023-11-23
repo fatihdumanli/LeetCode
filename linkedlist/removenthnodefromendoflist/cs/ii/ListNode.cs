e82682aea52ac59379e4bb0c064b880447707a90
Fatih Dumanli <fatihdumanli@live.com>
2023-11-23 16:44:55 +0300

    Add 'removenthnodefromtheendofthelist' recursive solution

---
+++ linkedlist/removenthnodefromendoflist/cs/ii/ListNode.cs
@@ -0,0 +1,12 @@
+namespace MyNameSpace;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val)
+    {
+        this.val = val;
+    }
+}
---
+++ linkedlist/removenthnodefromendoflist/cs/ii/Program.cs
@@ -0,0 +1,65 @@
+﻿using MyNameSpace;
+
+var head = new ListNode(1);
+head.next = new ListNode(2);
+
+var r = RemoveNthFromEnd(head, 2);
+
+Console.WriteLine(r.val);
+
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list
+ListNode RemoveNthFromEnd(ListNode head, int n) {
+
+    // Int32.MaxValoe is the result code for successful operation
+    // If not, it means the 'n' pointing to the head
+    if(Helper(head, n) != Int32.MaxValue)
+        head = head.next;
+
+    return head;
+}
+
+int Helper(ListNode head, int n)
+{
+    if(head == null)
+        return 0;
+
+    var r = Helper(head.next, n);
+
+    if(r == Int32.MaxValue)
+        return Int32.MaxValue;
+    
+    if(r == n)
+    {
+        head.next = head.next.next;
+        return Int32.MaxValue;
+    }
+
+    return r + 1;
+}
+
+//ListNode RemoveNthFromEnd(ListNode head, int n) {
+//
+//    // 1. get the size
+//    var ptr = head;
+//    var size = 0;
+//
+//    while(ptr != null)
+//    {
+//        size++;
+//        ptr = ptr.next;
+//    }
+//
+//    ptr = head;
+//
+//    for(int i = 0; i < size - n - 1; i++)
+//        ptr = ptr.next;
+//
+//    if(size - n == 0)
+//        head = head.next;
+//    else
+//        ptr.next = ptr.next.next;
+//
+//    return head;
+//}
+
+
