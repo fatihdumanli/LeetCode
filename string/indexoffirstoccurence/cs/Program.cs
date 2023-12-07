6512e6b798b299a8785e7bd0915b536b63a83c3a
Fatih Dumanli <fatihdumanli@live.com>
2023-12-07 20:33:54 +0300

    Add index of first occurence

---
+++ string/indexoffirstoccurence/cs/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+
+namespace IndexOfFirstOccurence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var haystack = "seboissad";
+            var needle = "sad";
+
+            var r = StrStr(haystack, needle);
+            Console.WriteLine(r);
+       }
+
+        static int StrStr(string haystack, string needle)
+        {
+            var hidx = 0;
+
+            for(int i = 0; i < needle.Length; i++)
+            {
+                // move the index on haystack
+                if(haystack[hidx] != needle[i])
+                {
+                    i = 0;
+                    hidx++;
+                    continue;
+                }
+                else
+                {
+                    // the current haystack char = current needle char
+                    // 
+                    if(hidx == needle.Length - 1)
+                        return hidx - needle.Length;
+
+                    hidx++;
+                }
+            }
+            
+            return -1;
+        }
+    }
+}
+
+
+
+
