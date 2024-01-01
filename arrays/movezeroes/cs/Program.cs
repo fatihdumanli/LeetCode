254a4dfda268cce487f9d4a417bfe3ef16e4b993
Fatih Dumanli <fatihdumanli@live.com>
2024-01-01 15:07:15 +0300

    Fix move zeroes

--- arrays/movezeroes/cs/Program.cs
+++ arrays/movezeroes/cs/Program.cs
@@ -1,40 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace cs;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        var nums = new int[] { 0, 1, 0, 3, 12 };
 
+        MoveZeroes(nums);
 
-// https://leetcode.com/problems/move-zeroes/
-var arr = new int[] {0,1,0,3,12};
+        Console.WriteLine("Hello, World!");
+    }
 
-// 1,3,12,0,0
-MoveZeroes(arr);
+    static void MoveZeroes(int[] nums)
+    {
+        var ptr = 0;
 
-void MoveZeroes(int[] nums) 
- {
-    int ptr = 0;
-
-    while (ptr < nums.Length) {
-        if (nums[ptr] != 0) {
-            ptr++;
-            continue;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+                nums[ptr++] = nums[i];
         }
 
-        ShiftLeft(nums, ptr);
-        nums[nums.Length - 1] = 0;
+        while(ptr < nums.Length)
+            nums[ptr++] = 0;
     }
 }
-
-void ShiftLeft(int[] nums, int from)
-{
-    for(int i = from; i < nums.Length - 1; i++) {
-        nums[i] = nums[i + 1];
-    }
-}
-
-void PrintArray(int[] nums)
-{
-    for(int i = 0; i < nums.Length; i++) 
-    {
-        Console.Write($"{nums[i]} ");
-    }
-}
-
