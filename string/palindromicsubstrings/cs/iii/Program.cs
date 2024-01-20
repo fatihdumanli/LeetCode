44e70112b7e6aacca36d3261fb6dceac676a90ec
Fatih Dumanli <fatihdumanli@live.com>
2024-01-20 18:56:27 +0300

    Add palindromic substrings1

---
+++ string/palindromicsubstrings/cs/iii/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace iii;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var s = "deeds";
+        var r = CountSubstrings(s);
+        Console.WriteLine(r);
+
+        Console.WriteLine("Hello, World!");
+    }
+
+    // https://leetcode.com/problems/palindromic-substrings/
+    static int CountSubstrings(string s)
+    {
+        var r = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            CountPalindromes(s, i, i + 1, ref r);
+            CountPalindromes(s, i, i, ref r);
+        }
+
+        return r;
+    }
+
+    static void CountPalindromes(string s, int left, int right, ref int r)
+    {
+        while(left >= 0 && right < s.Length && s[left] == s[right])
+        {
+            r++;
+            left--;
+            right++;
+        }
+    }
+}
