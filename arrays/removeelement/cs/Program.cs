c9781f7e7f97a4224e6c6f4e7432ad7eba29f273
Fatih Dumanli <fatihdumanli@live.com>
2024-03-02 16:49:11 +0300

    Add remove element

---
+++ arrays/removeelement/cs/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace cs;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var nums = new int[] {0,1,2,2,3,0,4,2};
+
+        var r = RemoveElement(nums, 2);
+
+        Console.WriteLine("Hello, World!");
+    }
+
+    // https://leetcode.com/problems/remove-element
+    static int RemoveElement(int[] nums, int val) 
+    {
+        var left = 0;
+        var right = 0;
+
+        while (right < nums.Length)
+        {
+            if (nums[right] != val)
+            {
+                nums[left++] = nums[right];
+            }
+
+            right++;
+        }
+
+        return left;
+    }
+}
