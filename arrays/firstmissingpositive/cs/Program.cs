3129c48764ad86c090246d4c2f2bde782ddb2dcf
Fatih Dumanli <fatihdumanli@live.com>
2024-03-26 20:20:23 +0300

    Update solution to use negative marking strategy

--- arrays/firstmissingpositive/cs/Program.cs
+++ arrays/firstmissingpositive/cs/Program.cs
@@ -1,16 +1,14 @@
-﻿using System.Diagnostics;
-using System.Numerics;
-
-namespace cs;
+﻿namespace cs;
 
 class Program
 {
     static void Main(string[] args)
     {
-        var nums = new int[] { 1, 2, 6, 3, 5, 4 };
+        // var nums = new int[] { 1, 2, 6, 3, 5, 4 };
         // var nums = new int[] { 2, 2, 2, 2, 2 };
         // var nums = new int[] { 3, 4, -1, 1 };
         // var nums = new int[] { 1, 1, 1, 1, 1 };
+        var nums = new int[] { 1, 1 };
 
         var r = FirstMissingPositive(nums);
 
@@ -20,108 +18,46 @@ class Program
     // https://leetcode.com/problems/first-missing-positive
     static int FirstMissingPositive(int[] nums)
     {
-        if (nums.Length == 1)
-        {
-            if (nums[0] == 1)
-                return 2;
-
-            return 1;
-        }
-
-        bool oneExists = false;
-        for (int i = 0; i < nums.Length; i++)
-        {
-            if (nums[i] == 1)
-                oneExists = true;
-        }
-
-        if (!oneExists)
-            return 1;
-
         for (int i = 0; i < nums.Length; i++)
         {
             var num = nums[i];
 
             if (num <= 0)
-                continue;
-
-            // The index is equal to the number, just mark as processed
-            if (num == i)
             {
-                nums[i] = GetProcessedMarker();
-                continue;
+                // cannot be considered - mark with n + 1
+                nums[i] = nums.Length + 1;
             }
+        }
 
-            // Either it means it's processed, or we've encountered a 'one'
-            // No need to traverse
-            if (nums[i] == GetProcessedMarker())
-            {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var num = Math.Abs(nums[i]);
+
+            // it cannot be considered - it was negative or zero
+            if (num > nums.Length)
                 continue;
-            }
+            // (ith element) represent the integer i + 1
+            // 0 -> 1 
+            // 1 -> 2 
+            // make the element negative (marking as exist)
 
-            // The number is positive
-            Traverse(nums, num);
+            // it's already negative - already marked as exists. 
+            // multiplying it with -1 one more time would result in
+            // unmarking the element
+            if (nums[num - 1] < 0)
+                continue;
+
+            nums[num - 1] = -1 * (nums[num - 1]);
         }
 
-        if (!oneExists)
-            return 1;
-
-        for (int i = 1; i < nums.Length; i++)
+        for (int i = 0; i < nums.Length; i++)
         {
-            if (nums[i] != GetProcessedMarker())
-                return i;
+            // if it's positive, that means the positive integer did not exist in the original array.
+            if (nums[i] > 0)
+                return i + 1;
         }
 
-        // We've reached the end of the array. 
-        // Meaning, the result is nums.Length
-
-        // What if nums[0] is equal to the length?
-        // Imagine array
-        // [3,2,1]
-        // Is result 3? 
-        // Nah, just because it's greater than the array size,
-        // It doesn't mean that we should just ignore it
-        // The answer should've been 4. Not 3.
-        // There's a logic in the Traverse() method, that allocates nums[0] for overflows.
-        // We're writing the minimum overflow to nums[0], so that we can compare it to the length of the array.
-        if (nums[0] == nums.Length)
-            return nums[0] + 1;
-
-        return nums.Length;
+        // all of them exist, return n + 1
+        return nums.Length + 1;
     }
-
-    static void Traverse(int[] nums, int num)
-    {
-        if (num > nums.Length - 1)
-        {
-            if (nums[0] <= 0)
-                nums[0] = num;
-            else if (nums[0] <= nums.Length - 1)
-                nums[0] = num;
-            else
-                nums[0] = Math.Min(nums[0], num);
-
-            return;
-        }
-
-        if (num <= 0)
-            return;
-
-        // meaning that's processed
-        var newNum = nums[num];
-
-        nums[num] = GetProcessedMarker();
-
-        // ignore duplicates
-        if (newNum == num || newNum == GetProcessedMarker())
-            return;
-
-        Traverse(nums, newNum);
-    }
-
-    static int GetProcessedMarker()
-    {
-        return (int)Math.Pow(2, 31);
-    }
-
 }
