f8f680394e3acfd0e8bdc2cb3090bf448f8ba3fd
Fatih Dumanli <fatihdumanli@live.com>
2024-06-14 11:23:55 +0300

    Add minimum number of moves to seat everyone

---
+++ arrays/minimumnumberofmovestoseateveryone/go/solution.go
@@ -0,0 +1,89 @@
+package main
+
+import "fmt"
+import "sort"
+
+func main() {
+    var seats = []int{3,20,17,2,12,15,17,4,15,20}
+    var students = []int{10,13,14,15,5,2,3,14,3,18}
+
+    r := minMovesToSeat(seats, students);
+
+    fmt.Println(r);
+}
+
+
+// https://leetcode.com/problems/minimum-number-of-moves-to-seat-everyone
+func minMovesToSeat(seats []int, students []int) int {
+
+    var seatBuckets = make([]int, 101);
+    var studentBuckets = make([]int, 101);
+
+    for i := 0; i < len(seats); i++ {
+        var idx = seats[i];
+        seatBuckets[idx]++;
+    }
+
+    for i := 0; i < len(students); i++ {
+        var idx = students[i];
+        studentBuckets[idx]++;
+    }
+
+    sort.Slice(seats, func(i, j int) bool {
+        return seats[i] <= seats[j];
+    });
+
+    sort.Slice(students, func(i, j int) bool {
+        return students[i] <= students[j];
+    });
+
+    // BEGIN
+
+    var result = 0;
+    var studentPtr = 0;
+
+    for i := 0; i < len(seats); i++ {
+
+        fmt.Println(seatBuckets);
+        fmt.Println(studentBuckets);
+
+        var seatIdx = seats[i];
+        var seatCount = seatBuckets[seatIdx];
+        var studentCount = studentBuckets[seatIdx];
+
+        if seatCount == studentCount {
+            continue;
+        }
+
+        if studentCount > seatCount {
+            continue;
+        }
+
+        for studentCount < seatCount {
+            var currentStudentIdx = students[studentPtr];
+
+            // we can pick him up
+            if studentBuckets[currentStudentIdx] > seatBuckets[currentStudentIdx] {
+                var diff = Abs(seatIdx - currentStudentIdx);
+                studentBuckets[currentStudentIdx]--;
+                studentBuckets[seatIdx]++;
+                result += diff;
+                studentPtr++;
+                studentCount = studentBuckets[seatIdx];
+            } else {
+                studentPtr++;
+            }
+        }
+    }
+
+
+    return result;
+}
+
+// Return only if the student is not seated
+func Abs(a int) int {
+    if a >= 0 {
+        return a;
+    }
+    return -a;
+}
