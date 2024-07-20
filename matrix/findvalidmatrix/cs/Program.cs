e184cdd52b6a042c66a8fe1a9976460f05ca85bf
Fatih Dumanli <fatihdumanli@live.com>
2024-07-20 21:53:09 +0300

    Add FindValidMatrix based on rowSum and colSum

---
+++ matrix/findvalidmatrix/cs/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace cs;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var rowSum = new int[] { 5, 7, 10 };
+        var colSum = new int[] { 8, 6, 8 };
+
+        var r = RestoreMatrix(rowSum, colSum);
+        Console.WriteLine(r);
+    }
+
+    // https://leetcode.com/problems/find-valid-matrix-given-row-and-column-sums/
+    static int[][] RestoreMatrix(int[] rowSum, int[] colSum) {
+        var result = new int[rowSum.Length][];
+
+        for (int i = 0; i < rowSum.Length; i++)
+            result[i] = new int[colSum.Length];
+        
+        for (int i = 0; i < rowSum.Length; i++)
+        {
+            for (int j = 0; j < colSum.Length; j++)
+            {
+                // Cell i, j
+                // The sum of the current row is rowSum[i]
+                // The sum of the current col is colSum[j]
+                var currentRowSum = rowSum[i];
+                var currentColSum = colSum[j];
+
+                var min = Math.Min(currentColSum, currentRowSum);
+
+                result[i][j] = min;
+
+                rowSum[i] -= min;
+                colSum[j] -= min;
+            }
+        }
+
+        return result;
+    }
+}
